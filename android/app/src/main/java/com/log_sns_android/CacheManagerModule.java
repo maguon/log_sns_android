849124ae37c416be1372a8e82567f8193d6c9f61
rbyu <rongbo.yu@myxxjs.com>
2020-04-30 16:54:35 +0800

    ok

---
+++ android/app/src/main/java/com/log_sns_android/CacheManagerModule.java
@@ -0,0 +1,7 @@
+package com.log_sns_android.CacheManager;
+
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+public class CacheManagerModule extends ReactContextBaseJavaModule {
+
+}
