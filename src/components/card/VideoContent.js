b7c07bffec1798f92560d787840b235444e14226
rbyu <rongbo.yu@myxxjs.com>
2020-06-08 23:55:08 +0800

    ok

--- src/components/card/VideoContent.js
+++ src/components/card/VideoContent.js
@@ -8,6 +8,7 @@ class VideoContent extends Component {
     }
 
     render() {
+       
         return (
             <TouchableOpacity style={{ flex: 1 }} onPress={() => {
 
@@ -18,15 +19,21 @@ class VideoContent extends Component {
                     controls={true}
                     ref={(ref) => {
                         this.player = ref
-                    }}                                      // Store reference
+                    }} 
+                                                         // Store reference
                     onEnd={() => {
                         this.player.seek(0);
                     }}
                     onError={(err) => {
-                        Alert.alert(JSON.stringify(err))
+                        console.log(err)
+                        console.log(this.props.video)
+                        
+                        // Alert.alert(JSON.stringify(err))
                     }}
-                    onLoad={() => {
-                        this.player.seek(0)
+                    onLoad={params => {
+                        console.log(params)
+                        console.log(this.props.video)
+                        
                     }}
                     style={styles.backgroundVideo} />
                 {/* <Image source={{uri:props.preview}} style={{width:200,height:200}}/> */}
